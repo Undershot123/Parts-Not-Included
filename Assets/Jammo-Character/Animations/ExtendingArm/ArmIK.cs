ebcf91c68ad722e5151137528e5bcf6ee6e7e41f
Undershot123 <carlos.2000.guerrero@gmail.com>
2021-12-02 21:33:00 -0700

    Added ArmIK.cs to allow extended arms to aim where camera is pointing to

--- Assets/Jammo-Character/Animations/ExtendingArm/ArmIK.cs
+++ Assets/Jammo-Character/Animations/ExtendingArm/ArmIK.cs
@@ -2,6 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class HumanBone
+{
+    public HumanBodyBones bone;
+}
+
 public class ArmIK : MonoBehaviour
 {
     //This script reassigns the position of Jammo's body to make him aim towards the object
@@ -9,40 +15,98 @@ public class ArmIK : MonoBehaviour
 
     public Transform targetTransform; //Empty object that Jammo's arm extends towards
     public Transform aimTransform; //Raycast node found in Jammo's forearm
-    public Transform bone;
+    public Transform aimHead;
 
     private ThirdPersonMovement movement;
     private Animator anim;
+
+    public int iterations = 10;
+    [Range(0,1)]
+    public float weight = 1.0f;
+
+    public Transform Shoulderbone;
+    public Transform Head;
+    public float angleLimit = 90.0f;
+    public float distanceLimit = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
         movement = this.GetComponent<ThirdPersonMovement>();
+        anim = movement.getAnimator();
+
+        /* boneTransforms = new Transform[humanBones.Length];
+        for(int i = 0; i < boneTransforms.Length; i++)
+        {
+            boneTransforms[i] = anim.GetBoneTransform(humanBones[i].bone);
+        } */
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        Vector3 targetPosition = targetTransform.position;
+        if(aimTransform == null || targetTransform == null)
+        {
+            return;
+        }
+        Vector3 targetPosition = GetTargetPosition();
+        
 
         anim = movement.getAnimator();
-        if(anim.GetBool("shoot"))
+        for(int i = 0; i < iterations; i++)
         {
-            delay(1.0f);
-            AimAtTarget(bone, targetPosition);
+            if(anim.GetBool("shoot"))
+            {
+                //delay(1.0f);
+                //Transform bone = boneTransforms[b];
+                anim.SetBool("shoot", true);
+            }
+            
+            AimAtTarget(Shoulderbone, targetPosition, weight); //Put back in if statement later
+            AimAtTarget(Head, targetPosition, weight);
         }
-        
     }
 
-    private void AimAtTarget(Transform bone, Vector3 targetPosition)
+    //Checks to see if Jammo is looking in a direction of 360 degrees, does not allow player to fire if jammo's camera is facing -90 to 90 degrees
+    Vector3 GetTargetPosition()
+    {
+        Vector3 targetDirection = targetTransform.position - aimTransform.position;
+        Vector3 aimDirection = aimTransform.forward;
+        float blendOut = 0.0f;
+
+        float targetAngle = Vector3.Angle(targetDirection, aimDirection);
+        
+        if(targetAngle > angleLimit)
+        {
+            blendOut += (targetAngle - angleLimit) / 50.0f;
+        }
+
+        float targetDistance = targetDirection.magnitude;
+        if(targetDistance < distanceLimit) //target is too close
+        {
+            blendOut += distanceLimit - targetDistance;
+        }
+
+        Vector3 direction = Vector3.Slerp(targetDirection, aimDirection, blendOut);
+        return aimTransform.position + direction;
+    }
+
+    private void AimAtTarget(Transform bone, Vector3 targetPosition, float weight)
     {
         Vector3 aimDirection = aimTransform.forward;
         Vector3 targetDirection = targetPosition - aimTransform.position;
         Quaternion aimTowards = Quaternion.FromToRotation(aimDirection, targetDirection);
-        bone.rotation = aimTowards * bone.rotation;
+        Quaternion blendedRotation = Quaternion.Slerp(Quaternion.identity, aimTowards, weight);
+        bone.rotation = blendedRotation * bone.rotation;
     }
 
-    IEnumerator delay(float time)
+    public void SetTargetTransform(Transform target)
     {
-        yield return new WaitForSeconds(time);
+        targetTransform = target;
+    }
+
+    public void SetAimTransform(Transform aim)
+    {
+        aimTransform = aim;
     }
 }
