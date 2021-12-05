344bbadcb098605eb91382fd3ab6624df640bed8
Jacob <jakel1227@gmail.com>
2021-12-05 14:37:13 -0700

    Start Menu Functionality v.2

--- Assets/Scripts/ButtonControl.cs
+++ Assets/Scripts/ButtonControl.cs
@@ -8,17 +8,26 @@ public class ButtonControl : MonoBehaviour
     // Start is called before the first frame update
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        Debug.Log("Play Game");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
     }
 
     public void InfoScreen()
     {
+        Debug.Log("InfoScreen");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void Back()
     {
+        Debug.Log("Back");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void Next()
+    {
+        Debug.Log("Next");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
